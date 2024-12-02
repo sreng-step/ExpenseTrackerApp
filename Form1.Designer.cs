025130e7d8442a9818b2e8d53678737ccf85aa97
srengty <srengty@gmail.com>
2024-12-02 20:04:46 +0700

    change title

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,24 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
+            LblTitle = new Label();
             SuspendLayout();
             // 
-            // label1
+            // LblTitle
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(228, 19);
-            label1.Name = "label1";
-            label1.Size = new Size(262, 30);
-            label1.TabIndex = 0;
-            label1.Text = "Expense tracker dashboard";
+            LblTitle.AutoSize = true;
+            LblTitle.Location = new Point(228, 19);
+            LblTitle.Name = "LblTitle";
+            LblTitle.Size = new Size(262, 30);
+            LblTitle.TabIndex = 0;
+            LblTitle.Text = "Expense tracker dashboard";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(760, 416);
-            Controls.Add(label1);
+            Controls.Add(LblTitle);
             Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             Margin = new Padding(5, 6, 5, 6);
             Name = "Form1";
@@ -56,6 +56,6 @@
 
         #endregion
 
-        private Label label1;
+        private Label LblTitle;
     }
 }
