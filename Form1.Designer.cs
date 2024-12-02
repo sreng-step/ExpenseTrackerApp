eb8aa59710e22e84b5e66ffdcf9c083853f38120
srengty <srengty@gmail.com>
2024-12-02 19:51:07 +0700

    Add title expense tracker (#task01)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(228, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(262, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Expense tracker dashboard";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(760, 416);
+            Controls.Add(label1);
             Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             Margin = new Padding(5, 6, 5, 6);
             Name = "Form1";
             Text = "Expense Tracker";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
     }
 }
